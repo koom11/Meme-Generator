4510c3f927ba1d3d1d454255bf8c42b45d8ee3c7
Eric K <kummer1122@gmail.com>
2020-07-22 19:44:43 -0400

    Meme Generator

---
+++ meme.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const img = document.querySelector('#img-url');
+    const topText = document.querySelector('#top-text');
+    const bottomText = document.querySelector('#bottom-text');
+    const submitBtn = document.querySelector('button');
+
+    submitBtn.addEventListener('click', function(e){
+        e.preventDefault();
+    })
+
+
+
+
+
+})
