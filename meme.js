0bd2bec42f2c097b75c7ca6698a43accfa6b6229
Eric K <kummer1122@gmail.com>
2020-07-22 20:05:02 -0400

    Changed CSS name and JS selectors

--- meme.js
+++ meme.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const img = document.querySelector('#img-url');
-    const topText = document.querySelector('#top-text');
-    const bottomText = document.querySelector('#bottom-text');
+    const img = document.createElement('div').classList.add('parent-img');
+    const topText = document.createElement('div').classList.add('meme-top');
+    const bottomText = document.createElement('div').classList.add('meme-bottom');
     const submitBtn = document.querySelector('button');
 
     submitBtn.addEventListener('click', function(e){
         e.preventDefault();
+        document.body.appendChild(img, topText, bottomText)
     })
+
+    
 
 
 
