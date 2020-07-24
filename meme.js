9c3a020898b5035aa71c888796ef62fc3c7eb295
Eric K <kummer1122@gmail.com>
2020-07-24 19:06:06 -0400

    Created divs for everything!

--- meme.js
+++ meme.js
@@ -1,14 +1,52 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const img = document.createElement('div').classList.add('parent-img');
-    const topText = document.createElement('div').classList.add('meme-top');
-    const bottomText = document.createElement('div').classList.add('meme-bottom');
+    const img = document.createElement('div');
+    const imgInput = document.querySelector('#img-url');
+    const imgURL = document.createElement('img');
+    const topTextInput = document.querySelector('#top-text');
+    const topText = document.createElement('div');
+    const bottomTextInput = document.querySelector('#bottom-text')
+    const bottomText = document.createElement('div');;
     const submitBtn = document.querySelector('button');
+    const memeList = document.querySelector('#meme-list');
+    const form = document.querySelector('form');
 
-    submitBtn.addEventListener('click', function(e){
+    form.addEventListener('submit', function(e){
+        /* e.preventDefault();
+        imgURL.src = imgInput.value;
+        img.appendChild(imgURL);
+        img.classList.add('parent-img')
+        memeList.appendChild(img);
+        topText.innerHTML = topTextInput.value;
+        topText.classList.add('meme-top');
+        img.appendChild(topText);
+        bottomText.innerHTML = bottomTextInput.value;
+        bottomText.classList.add('meme-bottom');
+        img.appendChild(bottomText); */
         e.preventDefault();
-        document.body.appendChild(img, topText, bottomText)
-    })
+        createParentDiv();
+        createTopTextDiv();
+        createBottomTextDiv();
+    
+    }, false);
 
+    function createParentDiv(){
+        imgURL.src = imgInput.value;
+        img.appendChild(imgURL);
+        img.classList.add('parent-img');
+        memeList.appendChild(img);
+    }
+
+    function createTopTextDiv(){
+        topText.innerHTML = topTextInput.value;
+        topText.classList.add('meme-top');
+        img.appendChild(topText);
+    }
+
+    function createBottomTextDiv(){
+        bottomText.innerHTML = bottomTextInput.value;
+        bottomText.classList.add('meme-bottom');
+        img.appendChild(bottomText);
+    }
     
 
 
