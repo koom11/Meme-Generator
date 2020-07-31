26f6fcc874f3911361adffb6684d7b1a3fdfc268
Eric K <kummer1122@gmail.com>
2020-07-31 19:16:26 -0400

    got multiple divs to be added to DOM, still not working though

--- meme.js
+++ meme.js
@@ -1,39 +1,58 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const img = document.createElement('div');
-    const imgInput = document.querySelector('#img-url');
-    const imgURL = document.createElement('img');
-    const topTextInput = document.querySelector('#top-text');
-    const topText = document.createElement('div');
-    const bottomTextInput = document.querySelector('#bottom-text')
-    const bottomText = document.createElement('div');
+    let newImg = document.createElement('li');
+    let imgInput = document.querySelector('#img-url');
+    let imgURL = document.createElement('img');
+    let topTextInput = document.querySelector('#top-text');
+    let topText = document.createElement('h1');
+    let bottomTextInput = document.querySelector('#bottom-text')
+    let bottomText = document.createElement('h1');
     const memeList = document.querySelector('#meme-list');
-    const form = document.querySelector('form');
+    let form = document.querySelector('form');
 
     form.addEventListener('submit', function(e){
         e.preventDefault();
-        createParentDiv();
-        createTopTextDiv();
-        createBottomTextDiv();
+        createNewDiv();
+        /* let newImg = document.createElement('div');
+        imgURL.src = imgInput.value;
+        newImg.appendChild(imgURL);
+        newImg.classList.add('parent-img');
+        topText.innerText = document.getElementById('top-text').value;
+        topText.classList.add('meme-top');
+        newImg.appendChild(topText);
+        bottomText.innerText = document.getElementById('bottom-text').value;
+        bottomText.classList.add('meme-bottom');
+        newImg.appendChild(bottomText);
+        memeList.appendChild(newImg); */
+        form.reset();
     
-    }, false);
+    });
 
     function createParentDiv(){
+        newImg = document.createElement('div');
         imgURL.src = imgInput.value;
-        img.appendChild(imgURL);
-        img.classList.add('parent-img');
-        memeList.appendChild(img);
+        newImg.appendChild(imgURL);
+        newImg.classList.add('parent-img');
+        memeList.appendChild(newImg);
     }
 
     function createTopTextDiv(){
         topText.innerHTML = topTextInput.value;
         topText.classList.add('meme-top');
-        img.appendChild(topText);
+        //newImg.append(topText);
     }
 
     function createBottomTextDiv(){
         bottomText.innerHTML = bottomTextInput.value;
         bottomText.classList.add('meme-bottom');
-        img.appendChild(bottomText);
+        //newImg.append(bottomText);
+    }
+
+    function createNewDiv(){
+        createParentDiv();
+        createTopTextDiv();
+        createBottomTextDiv();
+        memeList.appendChild(newImg).appendChild(topText);
+        memeList.appendChild(newImg).appendChild(bottomText);
     }
     
 
