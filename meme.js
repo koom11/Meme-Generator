e7460d8040c373e5d470cf13cb561bbe8dbdadd3
Eric K <kummer1122@gmail.com>
2020-08-06 20:07:30 -0400

    commiting finished product

--- meme.js
+++ meme.js
@@ -1,34 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     let newImg = document.createElement('li');
     let imgInput = document.querySelector('#img-url');
-    let imgURL = document.createElement('img');
     let topTextInput = document.querySelector('#top-text');
-    let topText = document.createElement('h1');
-    let bottomTextInput = document.querySelector('#bottom-text')
-    let bottomText = document.createElement('h1');
+    const bottomTextInput = document.querySelector('#bottom-text')
     const memeList = document.querySelector('#meme-list');
     let form = document.querySelector('form');
 
     form.addEventListener('submit', function(e){
         e.preventDefault();
         createNewDiv();
-        /* let newImg = document.createElement('div');
-        imgURL.src = imgInput.value;
-        newImg.appendChild(imgURL);
-        newImg.classList.add('parent-img');
-        topText.innerText = document.getElementById('top-text').value;
-        topText.classList.add('meme-top');
-        newImg.appendChild(topText);
-        bottomText.innerText = document.getElementById('bottom-text').value;
-        bottomText.classList.add('meme-bottom');
-        newImg.appendChild(bottomText);
-        memeList.appendChild(newImg); */
         form.reset();
     
     });
 
     function createParentDiv(){
-        newImg = document.createElement('div');
+        newImg = document.createElement('li');
+        const imgURL = document.createElement('img');
         imgURL.src = imgInput.value;
         newImg.appendChild(imgURL);
         newImg.classList.add('parent-img');
@@ -36,28 +23,22 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     function createTopTextDiv(){
+        const topText = document.createElement('h1');
         topText.innerHTML = topTextInput.value;
         topText.classList.add('meme-top');
-        //newImg.append(topText);
+        newImg.appendChild(topText);
     }
 
     function createBottomTextDiv(){
+        const bottomText = document.createElement('h1');
         bottomText.innerHTML = bottomTextInput.value;
         bottomText.classList.add('meme-bottom');
-        //newImg.append(bottomText);
+        newImg.append(bottomText);
     }
 
     function createNewDiv(){
         createParentDiv();
         createTopTextDiv();
         createBottomTextDiv();
-        memeList.appendChild(newImg).appendChild(topText);
-        memeList.appendChild(newImg).appendChild(bottomText);
     }
-    
-
-
-
-
-
-})
+});
